a5bbfcc20e9884943821f58f1f4a70e71bba7538
Maksym Del <max.del.edu@gmail.com>
2024-08-01 18:21:26 +0300

    min

--- setup.py
+++ setup.py
@@ -1,9 +1,7 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='sparse_autoencoder',
     version='0.0.1',
     url='https://github.com/MaksymDel/sparse_autoencoder.git',
-    packages=find_packages(),    
-    install_requires=['transformer_lens >= 1.9.1', 'torch >= 2.1.0', 'blobfile >= 2.0.2'],
 )
